13659ceb55cb1542a02f4cc844fa8741dc553d2c
CameronGeraats <47534956+CameronGeraats@users.noreply.github.com>
2020-01-31 15:53:52 -0500

    version1.0
    Added prior work to depository

---
+++ OpenGLEngine/OpenGLEngine/BungeeSystem.cpp
@@ -0,0 +1,151 @@
+#include "BungeeSystem.h"
+#include "TransformComponent.h"
+#include "ForceAccumulatorComponent.h"
+
+namespace Reality
+{
+	BungeeSystem::BungeeSystem()
+	{
+		requireComponent<BungeeComponent>();
+		//requireComponent<TransformComponent>();
+	}
+
+	void BungeeSystem::Update(float deltaTime)
+	{
+		for (auto e : getEntities())
+		{
+			auto& bungee = e.getComponent<BungeeComponent>();
+
+			// Paired Bungees
+			if (!e.hasComponent<TransformComponent>()) {
+			//if (bungee.connectedEntityB.hasComponent<TransformComponent>()) {
+		
+				if (bungee.connectedEntityA.hasComponent<TransformComponent>()
+					&& bungee.connectedEntityB.hasComponent<TransformComponent>())
+				{
+					float deltaL = 0;
+
+					auto& transformA = bungee.connectedEntityA.getComponent<TransformComponent>();
+					auto& transformB = bungee.connectedEntityB.getComponent<TransformComponent>();
+
+					Vector3 relativePosition = transformA.position - transformB.position;
+					float length = glm::length(relativePosition);
+
+					if (length <= bungee.restLength)
+					{
+						float g = 1.0f / (1.0f + pow(abs(0), 0.5f));
+						float r = 1 - g;
+
+						Color col = Color(r, g, 0, 1);
+
+						float deltaLength = length / 10.0f;
+						Vector3 direction = -glm::normalize(relativePosition);
+						for (int i = 0; i < 10; i++)
+						{
+							getWorld().data.renderUtil->DrawCube(
+								transformA.position + (float)i * deltaLength * direction,
+								Vector3(1.0f, 1.0f, 1.0f) * min((bungee.restLength / 20.0f), 100.0f), Vector3(0, 0, 0), col);
+						}
+						getWorld().data.renderUtil->DrawLine(transformA.position, transformB.position,
+							col);
+						return;
+					}
+
+					if (length > 0)
+					{
+						deltaL = length - bungee.restLength;
+
+						Vector3 force = -glm::normalize(relativePosition);
+						force *= bungee.springConstant * deltaL;
+
+						/*if (bungee.connectedEntityA.hasComponent<ForceAccumulatorComponent>())
+						{
+							bungee.connectedEntityA.getComponent<ForceAccumulatorComponent>().AddForce(force);
+						}*/
+						if (bungee.connectedEntityB.hasComponent<ForceAccumulatorComponent>())
+						{
+							bungee.connectedEntityB.getComponent<ForceAccumulatorComponent>().AddForce(-force);
+						}
+
+						float g = 1.0f / (1.0f + pow(abs(deltaL), 0.5f));
+						float r = 1 - g;
+
+						Color col = Color(r, g, 0, 1);
+
+						float deltaLength = length / 10.0f;
+						Vector3 direction = -glm::normalize(relativePosition);
+						for (int i = 0; i < 10; i++)
+						{
+							getWorld().data.renderUtil->DrawCube(
+								transformA.position + (float)i * deltaLength * direction,
+								Vector3(1.0f, 1.0f, 1.0f) * min((bungee.restLength / 20.0f), 100.0f), Vector3(0, 0, 0), col);
+						}
+						getWorld().data.renderUtil->DrawLine(transformA.position, transformB.position,
+							col);
+					}
+				}
+			}
+			else {
+				auto& bungeeTransform = e.getComponent<TransformComponent>();
+				if (bungee.connectedEntityA.hasComponent<ForceAccumulatorComponent>()
+					&& bungee.connectedEntityA.hasComponent<TransformComponent>())
+				{
+					float deltaL = 0;
+
+					auto& transformA = bungee.connectedEntityA.getComponent<TransformComponent>();
+					auto& forceAcc = bungee.connectedEntityA.getComponent<ForceAccumulatorComponent>();
+
+					Vector3 relativePosition = transformA.position - bungeeTransform.position;
+					float length = glm::length(relativePosition);
+
+					if (length <= bungee.restLength)
+					{
+						float g = 1.0f / (1.0f + pow(abs(deltaL), 0.5f));
+						float r = 1 - g;
+
+						Color col = Color(r, g, 0, 1);
+
+						float deltaLength = length / 10.0f;
+						Vector3 direction = -glm::normalize(relativePosition);
+						for (int i = 0; i < 10; i++)
+						{
+							getWorld().data.renderUtil->DrawCube(
+								transformA.position + (float)i * deltaLength * direction,
+								Vector3(1.0f, 1.0f, 1.0f) * min((bungee.restLength / 20.0f), 100.0f), Vector3(0, 0, 0), col);
+						}
+						getWorld().data.renderUtil->DrawLine(transformA.position, bungeeTransform.position,
+							col);
+						return;
+					}
+
+					if (length > 0)
+					{
+						deltaL = length - bungee.restLength;
+
+						Vector3 force = -glm::normalize(relativePosition);
+						force *= bungee.springConstant * deltaL;
+						forceAcc.AddForce(force);
+					}
+					float g = 1.0f / (1.0f + pow(abs(deltaL), 0.5f));
+					float r = 1 - g;
+
+					Color col = Color(r, g, 0, 1);
+
+					float deltaLength = length / 10.0f;
+					Vector3 direction = -glm::normalize(relativePosition);
+					for (int i = 0; i < 10; i++)
+					{
+						getWorld().data.renderUtil->DrawCube(
+							transformA.position + (float)i * deltaLength * direction,
+							Vector3(1.0f, 1.0f, 1.0f) * min((bungee.restLength / 20.0f), 100.0f), Vector3(0, 0, 0), col);
+					}
+					getWorld().data.renderUtil->DrawLine(transformA.position, bungeeTransform.position,
+						col);
+
+				}
+			}			
+
+			
+		}
+	}
+}
---
+++ OpenGLEngine/OpenGLEngine/BuoyancySystem.cpp
@@ -0,0 +1,62 @@
+#include "BuoyancySystem.h"
+#include "TransformComponent.h"
+#include "ForceAccumulatorComponent.h"
+
+namespace Reality
+{
+	BuoyancySystem::BuoyancySystem()
+	{
+		requireComponent<BuoyancyComponent>();
+
+	}
+
+	void BuoyancySystem::Update(float deltaTime)
+	{
+		for (auto e : getEntities())
+		{
+
+			auto& buoyancy = e.getComponent<BuoyancyComponent>();
+			auto& waterTransform = e.getComponent<TransformComponent>();
+
+			if (buoyancy.buoyEntity.hasComponent<TransformComponent>())
+			{
+				auto& buoyTransform = buoyancy.buoyEntity.getComponent<TransformComponent>();
+
+				getWorld().data.renderUtil->DrawLine(Vector3(0.0f, buoyancy.waterHeight + buoyancy.maxDepth, 0.0f), buoyTransform.position,
+					Color(1, 0, 0, 1));
+
+				//getWorld().data.renderUtil->DrawSphere(Vector3(0.0f), 30.0f, Color(0, 0, 1, 1));
+				getWorld().data.renderUtil->DrawCube(Vector3(0.0f, buoyancy.waterHeight + buoyancy.maxDepth - 15.0f, 0.0f), Vector3(50.0f, 30.0f, 50.0f), Vector3(0, 0, 0), Color(0, 0, 1, 1));
+
+				getWorld().data.renderUtil->RenderText(std::to_string(buoyancy.density), 0.0f, 0.0f, 1.0f, Color(1, 1, 1, 1));
+
+				float depth = buoyTransform.position.y;
+				Vector3 force = Vector3(0, 0, 0);
+
+				if (depth >= buoyancy.waterHeight + buoyancy.maxDepth)
+				{
+					return;
+				}
+
+				if (depth <= buoyancy.waterHeight - buoyancy.maxDepth)
+				{
+					force.y = buoyancy.density * buoyancy.volume;
+					if (buoyancy.buoyEntity.hasComponent<ForceAccumulatorComponent>())
+					{
+						buoyancy.buoyEntity.getComponent<ForceAccumulatorComponent>().AddForce(-force);
+					}
+					return;
+				}
+				
+				force.y = buoyancy.density * buoyancy.volume *
+					(depth - buoyancy.maxDepth - buoyancy.waterHeight) / 2 * buoyancy.maxDepth;
+				if (buoyancy.buoyEntity.hasComponent<ForceAccumulatorComponent>())
+				{
+					buoyancy.buoyEntity.getComponent<ForceAccumulatorComponent>().AddForce(-force);
+				}
+
+			}
+			
+		}
+	}
+}
