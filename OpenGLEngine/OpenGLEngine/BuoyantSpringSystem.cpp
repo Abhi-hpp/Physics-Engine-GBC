f62d4eceeae46aaf7fb609f8dc01a1a4182bfc13
b4tp3n9uin <Matthewmakepeace32@gmail.com>
2020-02-01 00:20:46 -0500

    Update BuoyantSpringSystem.cpp

--- OpenGLEngine/OpenGLEngine/BuoyantSpringSystem.cpp
+++ OpenGLEngine/OpenGLEngine/BuoyantSpringSystem.cpp
@@ -1,9 +1,5 @@
 #include "BuoyantSpringSystem.h"
 #include "ForceAccumulatorComponent.h"
-#include "TransformComponent.h"
-#include "BuoyantSpringComponent.h"
-
-#include "ParticleComponent.h"
 
 namespace Reality
 {
@@ -14,57 +10,44 @@ namespace Reality
 
 	void BuoyantSpringSystem::Update(float deltaTime)
 	{
+
+
 		for (auto e : getEntities())
 		{
-			auto& springTransform = e.getComponent<TransformComponent>();
-			auto& spring = e.getComponent<BuoyantSpringComponent>();
+			auto& entityTransform = e.getComponent<TransformComponent>();
+			auto& entityBuoyant = e.getComponent<BuoyantSpringComponent>();
+			auto& entityForceAcc = e.getComponent <ForceAccumulatorComponent>();
 
-			if (spring.connectedEntityA.hasComponent<TransformComponent>()
-				&& spring.connectedEntityB.hasComponent<TransformComponent>())
+			int i = 0;
+			i++;
+
+			getWorld().data.renderUtil->DrawCube(Vector3(0, -5, 50), Vector3(200, 10, 400));
+
+
+
+
+			float depth = entityTransform.position.y;
+
+			getWorld().data.renderUtil->DrawCube(entityTransform.position,
+				Vector3(entityBuoyant.maxDepth * 2.0f, entityBuoyant.maxDepth * 2.0f, entityBuoyant.maxDepth * 2.0f),
+				Vector3(0, 0, 0));
+
+			Vector3 force = Vector3(0.0f, 0.0f, 0.0f);
+
+			if (depth >= height + entityBuoyant.maxDepth)
 			{
-				auto& transformA = spring.connectedEntityA.getComponent<TransformComponent>();
-				auto& transformB = spring.connectedEntityB.getComponent<TransformComponent>();
-
-				Vector3 relativePosition = transformA.position - transformB.position;
-				float length = glm::length(relativePosition);
-				BuoyantSpringComponent water();
-
-				if (length > 0)
-				{
-					float deltaL = length - spring.restLength;
-					Vector3 force = -glm::normalize(relativePosition);
-					force *= spring.springConstant * deltaL;
-					//forceAcc.AddForce(force);
-
-
-					// Code Here:
-					if (transformA.position.y > 0.0f)
-					{
-						spring.connectedEntityA.getComponent<ForceAccumulatorComponent>().AddForce(force);
-					}
-					else if (transformA.position.y <= 0.0f)
-					{
-						spring.connectedEntityA.getComponent<ForceAccumulatorComponent>().AddForce(force);
-					}
-
-					float g = 1.0f / (1.0f + pow(abs(deltaL), 0.5f));
-
-					float r = 1 - g;
-
-					Color col = Color(r, g, 0, 1);
-
-					float deltaLength = length / 10.0f;
-					Vector3 direction = -glm::normalize(relativePosition);
-					for (int i = 0; i < 10; i++)
-					{
-						getWorld().data.renderUtil->DrawCube(
-							transformA.position + (float)i * deltaLength * direction,
-							Vector3(1.0f, 1.0f, 1.0f) * min((spring.restLength / 20.0f), 100.0f), Vector3(0, 0, 0), col);
-					}
-
-					getWorld().data.renderUtil->DrawLine(springTransform.position, transformA.position, col);
-				}
+				force.y = 0.0f;
 			}
+			else if (depth <= height - entityBuoyant.maxDepth)
+			{
+				force.y = density * entityBuoyant.volume;
+			}
+			else
+			{
+				force.y = density * entityBuoyant.volume * (entityBuoyant.maxDepth - depth - height);
+			}
+
+			entityForceAcc.AddForce(force);
 		}
 	}
 }
