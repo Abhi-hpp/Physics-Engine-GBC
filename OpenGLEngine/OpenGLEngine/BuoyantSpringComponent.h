4b6d92957c0a67f6f27df23be3de057621c370d1
b4tp3n9uin <Matthewmakepeace32@gmail.com>
2020-02-01 00:03:15 -0500

    Update BuoyantSpringComponent.h
    changed the buoyancy system

--- OpenGLEngine/OpenGLEngine/BuoyantSpringComponent.h
+++ OpenGLEngine/OpenGLEngine/BuoyantSpringComponent.h
@@ -5,24 +5,12 @@ namespace Reality
 {
 	struct BuoyantSpringComponent
 	{
-		BuoyantSpringComponent(float _springConstant = 10.0, float _restLength = 10.0f,
-			float _maxDepth = 20.0f, float _volume = 10.0f, float _waterHeight = 0.0f, float _liquidDensity = 20.0f, 
-			ECSEntity _connectedEntityA = ECSEntity(), ECSEntity _connectedEntityB = ECSEntity())
-			: springConstant(_springConstant), restLength(_restLength), maxDepth(_maxDepth), 
-			volume(_volume), waterHeight(_waterHeight), liquidDensity(_liquidDensity), 
-			connectedEntityA(_connectedEntityB), connectedEntityB(_connectedEntityB)
+		BuoyantSpringComponent(float _maxDepth = 5.0f, float _volume = 10.0f)
+			:maxDepth(_maxDepth), volume(_volume)
 		{
 
 		}
-		float springConstant;
-		float restLength;
-
 		float maxDepth;
 		float volume;
-		float waterHeight;
-		float liquidDensity;
-
-		ECSEntity connectedEntityA;
-		ECSEntity connectedEntityB;
 	};
 }
