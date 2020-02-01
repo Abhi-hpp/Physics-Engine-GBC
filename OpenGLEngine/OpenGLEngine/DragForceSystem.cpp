9ea80d29383c0f52c833ec7e0951d0c165a910ea
BEEMO197 <47534900+BEEMO197@users.noreply.github.com>
2020-02-01 00:41:02 -0500

    Submission for Assignment 1
    Functions for assignments are on lines 70 - 72

---
+++ OpenGLEngine/OpenGLEngine/DragForceSystem.cpp
@@ -0,0 +1,33 @@
+#include "DragForceSystem.h"
+
+namespace Reality
+{
+	DragForceSystem::DragForceSystem()
+	{
+		requireComponent<ForceAccumulatorComponent>();
+		requireComponent<ParticleComponent>();
+		requireComponent<DragForceComponent>();
+	}
+
+	void DragForceSystem::Update(float deltaTime)
+	{
+		for (auto e : getEntities())
+		{
+
+			auto& forceAcc = e.getComponent<ForceAccumulatorComponent>();
+			auto& particle = e.getComponent<ParticleComponent>();
+			auto& drag = e.getComponent<DragForceComponent>();
+
+			float velocityMag = glm::length(particle.velocity);
+
+			if (velocityMag > 0)
+			{
+				Vector3 force = -glm::normalize(particle.velocity);
+				force *= drag.k1 * velocityMag + drag.k2 * pow(velocityMag, 2);
+
+				forceAcc.AddForce(force);
+			}
+
+		}
+	}
+}
