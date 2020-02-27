0294a4e7a4413c760b294c9aa81ab0aba78f7a57
b4tp3n9uin <Matthewmakepeace32@gmail.com>
2020-02-27 16:15:17 -0500

    Update ResetPenetrationDeltaMoveSystem.cpp

--- OpenGLEngine/OpenGLEngine/ResetPenetrationDeltaMoveSystem.cpp
+++ OpenGLEngine/OpenGLEngine/ResetPenetrationDeltaMoveSystem.cpp
@@ -4,14 +4,15 @@ namespace Reality
 {
 	ResetPenetrationDeltaMoveSystem::ResetPenetrationDeltaMoveSystem()
 	{
-
+		requireComponent<PenetrationDeltaMoveComponent>();
 	}
 
 	void ResetPenetrationDeltaMoveSystem::Update(float deltaTime)
 	{
 		for (auto e : getEntities())
 		{
-			
+			auto& penetration = e.getComponent<PenetrationDeltaMoveComponent>();
+			penetration.deltaMove = Vector3(0, 0, 0);
 		}
 	}
 }
