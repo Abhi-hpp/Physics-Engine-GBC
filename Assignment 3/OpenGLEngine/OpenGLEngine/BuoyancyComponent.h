a4f86bf1c3c3ba444b7d598dddb83cd34acc21eb
inparia <60408315+inparia@users.noreply.github.com>
2020-03-31 22:22:24 -0400

    Update BuoyancyComponent.h

--- Assignment 3/OpenGLEngine/OpenGLEngine/BuoyancyComponent.h
+++ Assignment 3/OpenGLEngine/OpenGLEngine/BuoyancyComponent.h
@@ -8,8 +8,8 @@ namespace Reality {
 	{
 	public:
 
-		BuoyancyComponent(const Vector3& _center = Vector3(0,0,0), float _maxDepth = 100, float _volume = 1000, float _waterHeight = 450, float _liquidDensity = 0.8f):
-			maxDepth(_maxDepth), volume(_volume), waterHeight(_waterHeight), liquidDensity(_liquidDensity), centerOfBuoyancy(_center)
+		BuoyancyComponent(float _maxDepth = 100, float _volume = 1000, float _waterHeight = 450, float _liquidDensity = 0.8f):
+			maxDepth(_maxDepth), volume(_volume), waterHeight(_waterHeight), liquidDensity(_liquidDensity)
 		{
 
 		}
@@ -25,9 +25,6 @@ namespace Reality {
 		
 		// density
 		float liquidDensity;
-
-		// center of the force
-		Vector3 centerOfBuoyancy;
 
 	};
 }
